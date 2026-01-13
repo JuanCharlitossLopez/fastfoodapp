82526ded3ef5af1769a8d26e8f50883133614e7b
JuanCharlitossLopez <juancalopez825@gmail.com>
2026-01-13 17:21:04 -0600

    fix: update platform configuration to use environment variable

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,5 @@
 export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-  platform: 'com.jcll.fastfoodapp', 
+  platform: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_NAME, 
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
 }
