9fe8f4b5990e1d3c997ec5c3474c309511d39393
JuanCharlitossLopez <juancalopez825@gmail.com>
2026-01-14 15:01:35 -0600

    feat: implement authentication flow with sign-in and sign-up screens, including custom input and button components

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,7 @@
 export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_NAME, 
-  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
-}
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: '6967ea37000dc2550d6c',
+  userCollectionId: 'user',
+} 
